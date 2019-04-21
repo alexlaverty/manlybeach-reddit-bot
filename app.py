2461969044ac560880af9c0ab8d68a42e03b0a10
Laverty <alex.laverty@cba.com.au>
2019-04-21 12:54:42 +1000

    set waitfor random time between 1 and 1.5 hours

--- app.py
+++ app.py
@@ -224,7 +224,7 @@ def publish_to_reddit():
             url=row['url']
             print("Title : {}".format(title))
             print("URL : {}".format(url))
-            wait_for=randint(120, 180)
+            wait_for=randint(3600, 5400)
             try:
                 reddit.subreddit(sub_reddit).submit(title, url=url)
                 print("POSTED TO REDDIT!")
